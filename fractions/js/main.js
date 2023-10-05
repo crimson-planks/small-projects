200a0af328326f1c5947423914c5aa3956745a0c
crimson-planks <koreaminecraftmonster@gmail.com>
2023-10-05 16:20:09 -0600

    d

---
+++ fractions/js/main.js
@@ -0,0 +1,25 @@
+let denominator = 1, numerator = 1;
+function gcdF(a,b){
+    if(b==0) return a;
+    return gcdF(b, a%b);
+}
+function reduceFraction(){
+    let gcd = gcdF(numerator,denominator);
+    if(gcd!=1){
+        numerator/=gcd;
+        denominator/=gcd;
+        document.getElementById("numerator").innerHTML=numerator.toString();
+        document.getElementById("denominator").innerHTML=denominator.toString();
+    }
+    console.log(gcd);
+}
+function updateNumerator(){
+    numerator++;
+    reduceFraction();
+    document.getElementById("numerator").innerHTML=numerator.toString();
+}
+function updateDenominator(){
+    denominator++;
+    reduceFraction();
+    document.getElementById("denominator").innerHTML=denominator.toString();
+}
