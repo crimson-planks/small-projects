5883d629ca1094657a2d1dabbea3a7557905a9cc
crimson-planks <koreaminecraftmonster@gmail.com>
2024-09-16 19:37:42 -0600

    finalizing functions

---
+++ MM_pixelart/main.py
@@ -0,0 +1,4 @@
+from texture import MM_Texture
+from create_texture import encode_texture
+from read_texture import decode_texture
+from PIL import Image
--- MM_pixelart/unit_test.py
+++ MM_pixelart/unit_test.py
@@ -2,18 +2,22 @@ from PIL import Image
 from pathlib import Path
 import json
 from texture import MM_Texture
+import logging
+
+logger = logging.getLogger(__name__)
+
 test_image = Image.open(Path(__file__).resolve(True).parent / "num.png")
 test_MM_Texture = MM_Texture(**json.load((Path(__file__).resolve(True).parent / "num.json").open()))
 
 def test_same_image(img1: Image.Image, img2: Image.Image)->bool:
     return img1.convert("RGBA").tobytes()==img2.convert("RGBA").tobytes()
-def test_read_texture():
+def test_read_texture(logger: logging.Logger):
     import read_texture
     from texture import MM_Texture
     assert test_same_image(read_texture.decode_texture(test_MM_Texture),test_image)
-    print("read_texture pass!")
+    logger.info("read_texture pass!")
 
-def test_create_texture():
+def test_create_texture(logger: logging.Logger):
     import create_texture
     from texture import MM_Texture
     created_MM_Texture = create_texture.encode_texture(test_image,"num")
@@ -24,10 +28,12 @@ def test_create_texture():
             print(test_MM_Texture[index])
             print(created_MM_Texture[index])
             assert False
-    print("create_texture pass!")
+    logger.info("create_texture pass!")
 def main_test():
-    test_read_texture()
-    test_create_texture()
-    print("all unit tests pass!")
+    print()
+    logging.basicConfig(level=logging.INFO)
+    test_read_texture(logger)
+    test_create_texture(logger)
+    logger.info("all unit tests pass!")
 if __name__=='__main__':
     main_test()
