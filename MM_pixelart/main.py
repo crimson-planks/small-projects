1f85ecc1733b1604d7311d8bc505b28f6b6abe1c
crimson-planks <koreaminecraftmonster@gmail.com>
2024-09-17 18:36:29 -0600

    Update main.py

--- MM_pixelart/main.py
+++ MM_pixelart/main.py
@@ -1,4 +1,8 @@
 from texture import MM_Texture
 from create_texture import encode_texture
 from read_texture import decode_texture
-from PIL import Image
+from PIL import Image, ImageDraw
+img = Image.new("RGBA",(8,8),(0,0,0,0xff))
+d = ImageDraw.Draw(img)
+d.rectangle((0,0,3,3),(0xff,0xff,0xff,0xff))
+img.show()
