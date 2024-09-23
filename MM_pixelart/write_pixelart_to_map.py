8da4bcf02b021a7fc4c5e23b957d57312c9458ce
crimson-planks <koreaminecraftmonster@gmail.com>
2024-09-22 21:55:23 -0600

    Update write_pixelart_to_map.py

--- MM_pixelart/write_pixelart_to_map.py
+++ MM_pixelart/write_pixelart_to_map.py
@@ -4,6 +4,7 @@ import base64
 import zlib
 import numpy as np
 from PIL import Image
+from texture import MM_AnimatedPixelArt
 basepath = Path(__file__).resolve(True).parent
 framespath = basepath/"frames"
 with Image.open(framespath/"0.png") as init_image:
@@ -11,8 +12,9 @@ with Image.open(framespath/"0.png") as init_image:
 with open(basepath/"pixelart_data.bin","br") as compressed_data_file:
     decompressed_data = zlib.decompress(compressed_data_file.read())
     data_array = np.reshape(np.frombuffer(decompressed_data,np.uint8),(6507,block_amount[1],block_amount[0]))
-for frame in data_array:
-    for y in block_amount[1]:
-        for x in block_amount[0]:
-            pass
-    pass
+data_array.shape
+for y in range(block_amount[1]):
+    for x in range(block_amount[0]):
+        PixelArtID_csv = ','.join(hex(n)[2:] for n in data_array[0:100,y,x])
+        print()
+        MM_AnimatedPixelArt(4, 2, PixelArtID_csv, 1)
