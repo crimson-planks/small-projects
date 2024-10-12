070658430d9f69b3e413450d9deb64ba1dc2f3ef
crimson-planks <koreaminecraftmonster@gmail.com>
2024-10-12 13:07:27 -0600

    a

--- MM_pixelart/main.py
+++ MM_pixelart/main.py
@@ -1,9 +1,9 @@
 from pathlib import Path
-import video_to_frames, quantize, remove_artifacts, write_texture_to_map, write_CE_to_map
+import video_to_frames, quantize, remove_artifacts, write_texture_to_map, write_pixelart_to_map
 file_path = Path(input("\nfile path:"))
-video_to_frames.main()
-quantize.main()
+#video_to_frames.main()
+#quantize.main()
 remove_artifacts.main(file_path)
 write_texture_to_map.main(file_path)
-write_CE_to_map.main(file_path)
+write_pixelart_to_map.main(file_path)
 print("Done!")
---
+++ MM_pixelart/write_note_CE_template.py
@@ -0,0 +1,31 @@
+playing_CEtemplate = {
+    "Behavior" : 0,
+    "CanPlayerStandOn" : True,
+    "HitBoxHeight" : 16,
+    "HitBoxWidth" : 48,
+    "ID" : None,
+    "PixelArtID" : "0",
+    "TransformCondition" : 2,
+    "TransformConditionValue" : 18,
+    "TransformDelay" : 0,
+    "TransformID" : None
+},
+not_playing_CEtemplate = {
+    "Behavior" : 0,
+    "CanPlayerStandOn" : True,
+    "HitBoxHeight" : 16,
+    "HitBoxWidth" : 16,
+    "ID" : None,
+    "PixelArtID" : "1",
+    "TransformCondition" : 2,
+    "TransformConditionValue" : 7,
+    "TransformDelay" : 0,
+    "TransformID" : None
+},
+end_CEtemplate = {
+    "Behavior" : 0,
+    "CanHurtPlayer" : False,
+    "CanInteractWithEnemies" : False,
+    "ID" : "end",
+    "PixelArtID" : "b"
+}
