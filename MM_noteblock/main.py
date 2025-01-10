606d90a44327831a2fc7e5c4db1fc8c0169dd8a1
crimson-planks <koreaminecraftmonster@gmail.com>
2025-01-09 23:28:48 -0600

    Sr

--- MM_noteblock/main.py
+++ MM_noteblock/main.py
@@ -4,6 +4,7 @@
 #2. create CE templates for each of the rhythms.
 import dataclasses
 import enum
+import json
 #notecontraption: the contraption that plays the same note multiple times
 @dataclasses.dataclass(slots = True)
 class MM_notecontraption:
@@ -44,24 +45,40 @@ end_CEtemplate = {
     "Behavior" : 0,
     "CanHurtPlayer" : False,
     "CanInteractWithEnemies" : True,
+    "CanPlayerStandOn" : True,
+    "HitBoxHeight" : 16,
+    "HitBoxWidth" : 48,
     "ID" : "end",
-    "PixelArtID" : "b"
+    "PixelArtID" : "0"
 }
 lengths = [(),(),()]
-onenote_rslt = []
-onenote_rslt.append(not_playing_CEtemplate.copy())
+onenote_rslt = [end_CEtemplate.copy()]
+
+def get_normalized_note_length_list(note_length_list: list[int]):
+    pass
 def get_rhythm_CEtemplate_list(start_frame: int, note_length_list: list[int], prefix: str):
+    result = []
     first_np = not_playing_CEtemplate.copy()
     first_np["ID"] = prefix + "np_pre"
     first_np["TransformConditionValue"] = start_frame
     first_np["TransformID"] = prefix + "p0"
+    result.append(first_np)
 
     for i,note_length in enumerate(note_length_list):
         p = playing_CEtemplate.copy()
         p["ID"] = prefix + "p" + hex(i)[2:]
-        p["TransformID"] = prefix + "np" + hex(i)[2:]
-
+        if i == len(note_length_list)-1: p["TransformID"] = "end"
+        else: p["TransformID"] = prefix + "np" + hex(i)[2:]
+        result.append(p)
+        if i == len(note_length_list)-1: break
         np = not_playing_CEtemplate.copy()
         np["ID"] = prefix + "np" + hex(i)[2:]
-        p["TransformID"] = prefix + "p" + hex(i)[2:]
-print(onenote_rslt)
+        np["TransformID"] = prefix + "p" + hex(i+1)[2:]
+        assert note_length-PLAYING_CE_DURATION>0
+        np["TransformConditionValue"] = note_length-PLAYING_CE_DURATION
+        result.append(np)
+    return result
+with open("MM_noteblock/result.json","w") as result_file:
+    onenote_rslt.extend(get_rhythm_CEtemplate_list(20,[60,60,60,60,60,60],""))
+    json.dump(onenote_rslt,result_file,indent=4)
+#man I just want to play Bad Apple but I have to code
